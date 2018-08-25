fe108eec6c2fd6fb7dc1cfa7e79c803194d44936
Zsolt Meszaros <zsolt@sowad.com>
2018-08-25 09:33:24 +0200

    Add tests for /api/filedata endpoint

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const path = require('path');
 const request = require('supertest');
 
 const app = require('../app');
@@ -18,11 +19,48 @@ describe('Endpoints', () => {
     });
   });
 
-  describe('POST /filedata', () => {
-    it.skip('should return a JSON object with the file size, name and MIME type', () => {});
+  describe('POST /api/filedata', () => {
+    it('should return a JSON object with the file size, name and MIME type', (done) => {
+      request(app)
+        .post('/api/filedata')
+        .attach('upfile', path.join(__dirname, './file-to-upload.txt'))
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((res) => {
+          expect(res.body).toEqual(
+            expect.objectContaining({
+              name: 'file-to-upload.txt',
+              size: 38,
+              type: 'text/plain',
+            })
+          );
+          done();
+        });
+    });
 
-    it.skip('should return an error if no file is uploaded', () => {});
+    it('should return an error if no file is uploaded', (done) => {
+      request(app)
+        .post('/api/filedata')
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.error).toBeTruthy();
+          expect(res.body.message).toBeDefined();
+          done();
+        });
+    });
 
-    it.skip('should return an error if field name is not upfile', () => {});
+    it('should return an error if field name is not upfile', (done) => {
+      request(app)
+        .post('/api/filedata')
+        .attach('wrongFieldName', path.join(__dirname, './file-to-upload.txt'))
+        .expect('Content-Type', /json/)
+        .expect(400)
+        .then((res) => {
+          expect(res.body.error).toBeTruthy();
+          expect(res.body.message).toBeDefined();
+          done();
+        });
+    });
   });
 });
